00916734e8b01aae89bfc2855c6a4e45ad0651e6
Alejandra Izquierdo <allleizq@gmail.com>
2022-07-04 13:17:48 -0700

    used css to style modal

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+    // var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+    // function displayMovieInfo() {
+        
+        // var weatherDay = $(this).attr("data-name");
+        // var queryURL = "https://www.omdbapi.com/?t=" +  movie +"&plot=short&apikey=trilogy";
+
+        // // Creating an AJAX call for the specific movie button being clicked
+//         $.ajax({
+//           url: queryURL,
+//           method: "GET"
+//         }).then(function(response) {
+//           $("#movies-view").empty();
+
+// }
+
+http://openweathermap.org/api
+
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=San%20Francisco&units=imperial&appid=a332184f8c6ec6a979de62cfa465345f"
+
+
+
