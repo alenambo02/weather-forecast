dc247386cbf3708a857d93dc46f4c0f64466b2b0
Alejandra Izquierdo <allleizq@gmail.com>
2022-07-04 18:14:28 -0700

    working on linking api

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,36 @@
+// var seachCont = $("#searchCont")
+// var button = $("#addItemBtn");
+
+$(document).on("click", "#addItemBtn", displayCityWeathers);
+
+function displayCityWeathers() {
+    var cities = $(this).attr("data-name");
+    var queryURl = "https://api.openweathermap.org/data/2.5/weather?q=San%20Francisco&units=imperial&appid=a332184f8c6ec6a979de62cfa465345f"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        $("#weatherDisplayHere").empty();
+
+
+
+    })
+}
 
 
 
 
 
-    // var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+// function displayCityWeathers() {
 
-    // function displayMovieInfo() {
-        
-        // var weatherDay = $(this).attr("data-name");
-        // var queryURL = "https://www.omdbapi.com/?t=" +  movie +"&plot=short&apikey=trilogy";
 
-        // // Creating an AJAX call for the specific movie button being clicked
-//         $.ajax({
-//           url: queryURL,
-//           method: "GET"
-//         }).then(function(response) {
-//           $("#movies-view").empty();
+// var queryURl = "https://api.openweathermap.org/data/2.5/weather?q=San%20Francisco&units=imperial&appid=a332184f8c6ec6a979de62cfa465345f"
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+//     $("#movies-view").empty();
 
 // }
-
-http://openweathermap.org/api
-
-
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=San%20Francisco&units=imperial&appid=a332184f8c6ec6a979de62cfa465345f"
-
-
-
