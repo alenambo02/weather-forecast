822951aa3eaf0efd42c8109f36291b650ae66f5e
Alejandra Izquierdo <allleizq@gmail.com>
2022-07-06 08:08:09 -0700

    updated read me

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # weather-forecast
+
+## Description
+Hello, Here you will find a weather dashboard that allows you to search any city and display its current temperature, humidity, wind-speed, and uv index. Right below it, you can access a 5-day weather forecast of the city you have searched. These weather icons will display the date, temperature, humidity, and an icon that matches the current weather for that day.  
+
+Ideas:
+
+Challenges: 
+
+## Usage
+You may use this page to access information about local storage, how to call on different API's using Javascript and the fetch method. Feel free to reach out to me with ways I can simplify my code or better it! I have included an image reference to the page and a deployed link of the webpage below: ------> https://alenambo02 <------
+
+```alt text
+
+Also I included a code snippet of how to call on an API using the fetch method below:
+
+
+```
+
+## License 
+MIT License
+
+Copyright (c) 2022 Ale
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Credits
+Helpful tool to understand how to display data retrieved from API to HTML: 
+https://jack72828383883.medium.com/how-to-use-javascript-fetch-to-display-api-results-in-html-7aa59936ed30
+https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+
+Helpful video/tools to understand calling on API's: https://www.youtube.com/watch?v=WZNG8UomjSI&t=1467
+https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,105 @@
 var forecastBox = $("#forecastBox");
 var weatherBox = $("#weatherBox");
 var form = $("#cityForm");
-var formInput = $("#cityInput");
+var searchInput = $("#searchInput");
 var button = $("#addItemBtn");
+var temperatureItem = $(".temperature");
+var humidityItem = $(".humidity");
+var windSpeedItem = $(".wind");
+var uvIndexItem = $(".uvIndex");
 
 
 
-$(document).on("click", "#addItemBtn", displayCityWeathers);
-var apiKey = "a332184f8c6ec6a979de62cfa465345f"
-var city = "chicago"
+// var apiKey = "a332184f8c6ec6a979de62cfa465345f"
 
-
-function displayCityWeathers(event) {
+$(document).on("click", "#addItemBtn", function (event) {
     event.preventDefault();
     console.log("working")
- 
-    var latLonURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey 
+    // var cityInput = cityInputElement.val();
+    // if (cityInput === "") {
+    //     return;
+    // }
+    // searchInput = cityInput;
+    // val = cityInput;
 
-     console.log(latLonURL)
+    cityInputElement.val("");
+    fetchWeather();
 
-    fetch(latLonURL)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.list[0])
-        console.log(data.list[1])
-        console.log(data.list[2])
-        console.log(data.list[3])
-        console.log(data.list[4])
-    });  
+
+});
+
+function displayingItemsBox2(temperatureItem, humidityItem, windSpeedItem, uvIndexItem, cityInput) {
+    var weatherBoxDiv = document.querySelector("#weatherbox");
+
+}
+
+function fetchWeather(lat, lon, cityInput) {
+  
+        var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=a332184f8c6ec6a979de62cfa465345f"
+            console.log(oneCall)
+        fetch(oneCall)
+        .then(response => response.json())
+        .then(data => { 
+        //grabbing data from object array
+            temperatureItem = data.current.temp;
+            humidityItem = data.current.humidity;
+            windSpeedItem = data.current.wind_speed;
+            uvIndexItem = data.current.uvi;  
+
+            var
+
+            
+        
+    });
+    
 }
 
 
+// // Function for geocoding 
 
-// Function for geocoding 
-// var latLonURL = "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=apiKey"
-// function useGeocodeForCity(event) {
-//     var lat = 0;
-//     var lon = 0;
+function useGeocodeForCity() {
+    var lat = 0;
+    var lon = 0;
+    
+   cityInput = searchInput.val();
+    
+    var cityURL = "http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=a332184f8c6ec6a979de62cfa465345f"
+        console.log(cityURL)
+    fetch(cityURL)
+    .then(response => response.json())
+    .then(data => {
+        lat = data[0].lat;
+        lon = data[0].lon;
 
-//     var cityName = cityInput.val();
-//     var cityURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey
-//     console.log(cityURL)
-//     fetch(cityURL)
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-        
-//     });  
+        fetchWeather(lat, lon, cityInput)
+    })
+    searchInput.val("");
+}
+
+ 
+
 
 
  // `api.openweathermap.org/data/2.5/forecast?q=chicago&appid=a332184f8c6ec6a979de62cfa465345f`
+//  var latLonURL = "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=apiKey"
+
+
+// function displayCityWeathers(event) {
+//     event.preventDefault();
+//     console.log("working")
+// }
+ 
+//     var latLonURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey 
+
+//      console.log(latLonURL)
+
+//     fetch(latLonURL)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data.list[0])
+//         console.log(data.list[1])
+//         console.log(data.list[2])
+//         console.log(data.list[3])
+//         console.log(data.list[4])
+//     });  
+// }
