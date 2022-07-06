aa25b54f73a1d2ce40dd8d6785ee0c6931de23ff
Alejandra Izquierdo <allleizq@gmail.com>
2022-07-05 21:12:18 -0700

    created event listener and function for it

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,25 @@
-// var seachCont = $("#searchCont")
-// var button = $("#addItemBtn");
+var forecastBox = $("#forecastBox");
+var weatherBox = $("#weatherBox");
+var form = $("#cityForm");
+var formInput = $("#cityInput");
+var button = $("#addItemBtn");
+
+
 
 $(document).on("click", "#addItemBtn", displayCityWeathers);
 var apiKey = "a332184f8c6ec6a979de62cfa465345f"
 var city = "chicago"
-// var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=a332184f8c6ec6a979de62cfa465345f`
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey
+
+
 function displayCityWeathers(event) {
     event.preventDefault();
     console.log("working")
-    // var cities = $(this).attr("data-name");
-    
-    
-   console.log(queryURL)
-    fetch(queryURL)
+ 
+    var latLonURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey 
+
+     console.log(latLonURL)
+
+    fetch(latLonURL)
     .then(response => response.json())
     .then(data => {
         console.log(data.list[0])
@@ -26,18 +32,21 @@ function displayCityWeathers(event) {
 
 
 
+// Function for geocoding 
+// var latLonURL = "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=apiKey"
+// function useGeocodeForCity(event) {
+//     var lat = 0;
+//     var lon = 0;
+
+//     var cityName = cityInput.val();
+//     var cityURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey
+//     console.log(cityURL)
+//     fetch(cityURL)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+        
+//     });  
 
 
-// function displayCityWeathers() {
-
-
-// var queryURl = "https:
-
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//     $("#movies-view").empty();
-
-// }
  // `api.openweathermap.org/data/2.5/forecast?q=chicago&appid=a332184f8c6ec6a979de62cfa465345f`
